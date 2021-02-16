a5e307b7e97fc2d0aaec7fbcb3e03d762532ba75
psychomanouel10 <74877599+psychomanouel10@users.noreply.github.com>
2021-02-16 08:27:19 +0200

    Add files via upload

---
+++ commands/Games/roll.js
@@ -0,0 +1,51 @@
+const Command = require('../../base/Command.js');
+const { MessageEmbed } = require('discord.js');
+
+class Roll extends Command {
+  constructor(client) {
+    super(client, {
+      name: "roll",
+      description: "Bet of the outcome of rolling a dice.",
+      category: "Games",
+      usage: "roll <one / two / three / four / five / six>"
+    });
+  }
+
+  async run(message, args) { 
+
+    if (!args.length) {
+      return message.reply("Command Usage: `roll < one / two / three / four / five / six >`")
+    }
+
+      let argsoutcome = args[0];
+      let outcomes = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six'
+      ];
+      let outcome = outcomes[Math.floor(Math.random() * outcomes.length)];
+
+      let result;
+      if (outcome.toLowerCase() === argsoutcome.toLowerCase()) {
+        result = 'Congratulations! You won the bet.';
+      } else {
+        result = 'Sorry, you lost the bet. Better luck next time.';
+      }
+
+      const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(`Rolled: ${outcome}:`)
+        .setDescription(result)
+      await message.channel.send({ embed
+      }).catch(e => {
+        this.client.logger.error(e);
+        return message.channel.send(`An error occurred:\n\```${error.message}\````);
+      });
+    }
+  };
+
+
+module.exports = Roll;
---
+++ commands/Games/slots.js
@@ -0,0 +1,52 @@
+const Command = require('../../base/Command.js');
+const { MessageEmbed } = require('discord.js');
+
+class Slots extends Command {
+  constructor(client) {
+    super(client, {
+      name: "slots",
+      description: "Spin the slot machine! You win if all 3 reels stop at the same emojis.",
+      category: "Games",
+      usage: "slots",
+      aliases: ["slot-machine"]
+    });
+  }
+
+  async run(message, args) { 
+      let reel = [
+        ':custard:',
+        ':candy:',
+        ':cake:',
+        ':icecream:',
+        ':lollipop:',
+        ':chocolate_bar:',
+        ':moneybag:',
+        ':shaved_ice:',
+        ':doughnut:',
+        ':cookie:',
+        ':ice_cream:'
+      ];
+
+      let reels = [];
+      for (let i = 0; i < 3; i++) {
+        reels.push(reel[Math.floor(Math.random() * reel.length)]);
+      }
+
+      let result;
+      if (reels[0] === reels[1] && reels[1] === reels[2]) {
+        result = 'Congratulations! You won.';
+      } else {
+        result = 'Sorry, you lost. Better luck next time.';
+      }
+
+      const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle('Slot Machine')
+        .setDescription(reels.join(' \u05C0 '))
+        .setFooter(`🎰 ${result}`)
+      await message.channel.send({ embed });
+    }
+  };
+
+
+module.exports = Slots;
